779f7765f131223cccfe36f3b24bb66483908fe8
Gui-Parreira <gui.parreira98@gmail.com>
2024-05-30 15:48:34 -0300

    Final implementação da regra de negócios Dev, inicio testes Main

--- src/Dev.java
+++ src/Dev.java
@@ -1,5 +1,6 @@
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -8,15 +9,24 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não esta matriculado em nenhum conteúdo!");
+        }
     }
 
     public double calcularXp() {
-        return 0;
+        return this.conteudosConcluidos.stream()
+                .mapToDouble(Conteudo::carcularXp)
+                .sum();
     }
 
     public String getNome() {
